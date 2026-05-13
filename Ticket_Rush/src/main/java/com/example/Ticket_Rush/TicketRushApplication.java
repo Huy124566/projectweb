75de730faa44d2be1c9b35e6039b3f69987a3ec9
Quang Minh <huy01674188280@gmail.com>
2026-05-13 23:10:23 +0700

    Fix: Switch to Resend API and update application properties

--- Ticket_Rush/src/main/java/com/example/Ticket_Rush/TicketRushApplication.java
+++ Ticket_Rush/src/main/java/com/example/Ticket_Rush/TicketRushApplication.java
@@ -1,8 +1,12 @@
 package com.example.Ticket_Rush;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 @EnableScheduling
@@ -10,6 +14,12 @@ public class TicketRushApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketRushApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void init() {
+	    // Ép toàn bộ ứng dụng chạy múi giờ Việt Nam
+	    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 	}
 
 }
